6c0720b2f87edcbaa03c7758b86002fce6c2f00f
unknown <zuriel2005@hotmail.com>
2014-11-27 15:03:13 -0600

    hola

---
+++ js/encuesta.js
@@ -0,0 +1,82 @@
+(function( $ ){
+
+  var methods = {
+      
+
+      init: function( o ){
+        o.show.html( o.preguntas[ o.pos ] );
+      },
+      changeQuestion : function( o ){
+        o.pos++;
+        o.show.html( o.preguntas[ o.pos ] );
+      },
+      changeBck: function( o ){
+        o.body.css( 'background', o.bg );
+      }
+     
+  };//methods
+
+
+  $.core = function( options ){
+
+    //Variables que se pueden utilizar dentro de nuestra aplicación
+    var settings = {
+
+      pos: 0,
+      show: $("#caja"),
+      bg: '#ffff00',
+      body: $( 'body' ),
+      preguntas: [ "",
+        "¿Quién eres?", "¿Qué te gusta hacer?", "preg 3", "Preg 4"       
+      ],
+      result:[
+      ]
+
+
+    }//settings
+
+    var o = $.extend( settings, options );
+
+
+      //======================================
+      //      Programando Actividad
+      //======================================
+      methods.init( o );
+
+      $( '#fondo' ).click(function(){
+        methods.changeBck( o );
+      });
+
+      $( '#sig' ).click(function(){
+        
+        if (o.pos < o.preguntas.length - 1){
+          methods.changeQuestion( o );
+        }else{
+          o.show.html( 'has terminado tu quiz' );
+        }
+
+      });
+
+      $(".resp").click(function(){
+        var resp = parseInt($(this).text());
+       o.result.push(resp);
+       console.log( o.result );
+
+      });
+
+      $("#sig").click(function(){
+      $("#caja").animate({
+    left:'750px',
+    height:'50px',
+    width:'350px'
+});
+
+});     
+  
+    
+  
+
+      
+  }//core functions
+
+})( jQuery );
